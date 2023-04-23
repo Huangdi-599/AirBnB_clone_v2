d2ea9fa27c52442db68e29f52278cb052504e12c
Huangdi-599 <92282845+Huangdi-599@users.noreply.github.com>
2023-04-23 21:40:02 +0100

    task 1 to 6

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -2,8 +2,11 @@
 """a script that starts a Flask web application:
 The web application listens on 0.0.0.0, port 5000.
 Routes:
-    /: 
-Displays 'Hello HBNB!'
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /number/<n>: Displays 'n is a number' only if <n> is an integer.
 """
 from flask import Flask
 
