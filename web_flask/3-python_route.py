23d6bb93b7581821a0026b3a8f20662a3e53d134
Huangdi-599 <92282845+Huangdi-599@users.noreply.github.com>
2023-04-23 22:24:20 +0100

    done

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application:
-The web application listens on 0.0.0.0, port 5000.
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'.
     /hbnb: Displays 'HBNB'.
@@ -14,20 +14,26 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'."""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
-def hbnb(text):
-    """display “C ” followed by the value of the text variable (replace underscore _ symbols with a space)"""
+def c(text):
+    """Displays 'C' followed by the value of <text>.
+    Replaces any underscores in <text> with slashes.
+    """
     text = text.replace("_", " ")
     return "C {}".format(text)
 
+
+@app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
     """Displays 'Python' followed by the value of <text>.
